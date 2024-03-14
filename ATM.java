b2aefeddbe2e97e04f2a0bcd6f0dfd9eea907103
Ricky Johnson <rickyjohnson2018@gmail.com>
2024-03-14 16:14:06 -0400

    something

--- ATM.java
+++ ATM.java
@@ -1,14 +1,80 @@
 //pa1
 
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Scanner;
 
-public class ATM { //remember to change the name of the class to match the name of the file.
 
+public class ATM {
     public static void main(String[] args){
 
-     
-        System.out.print("Let's Play BlackJack! ");
+        // Scanner object
+        Scanner input = new Scanner(System.in);
 
-    
+        // Create DecimalFormat object
+        DecimalFormat currency = new DecimalFormat("$#,##0.00");
+
+        // Introduce program
+        System.out.println();
+        System.out.println("Welcome to the ATM.");
+        System.out.println();
+
+        // Ask user for first name
+        System.out.println("Enter your first name: ");
+        String firstName = input.nextLine();
+
+        // Ask user for last name
+        System.out.println("Enter your last name: ");
+        String lastName = input.nextLine();
+
+        // Ask user for account number
+        System.out.println("Enter account number: ");
+        int accountNumber = input.nextInt();
+        // Error check for input of 0. 0 is default value.
+        while (accountNumber <= 0) {
+            System.out.println("\nAccount number must be a number larger than 0 and a whole number (no decimals).");
+            System.out.println("Enter account number: ");
+            accountNumber = input.nextInt();
+        }
+
+
+        // Instantiate array of account numbers
+        int[] accountNumbers = new int[5];
+        // Instantiate array of account objects
+        Account[] accountObjects = new Account[5];
+
+        // for loop to check if user entered account number exists
+        for (int i = 0; i < accountNumbers.length; i++) {
+            if (accountNumbers[i] == 0) {   // 0 is default value. Any index with 0 is space for a new account.
+                accountNumbers[i] = accountNumber;  // index of accountNumbers becomes user entered account number
+                break;  // break loop so other indexes with 0 are not replaced
+            }
+        }
+
+        // For loop to add new account object to accountObjects. Index of accountObjects corresponds with index of accountNumbers.
+        for (int j = 0; j < accountObjects.length; j++) {
+            if (accountObjects[j] == null) {
+                accountObjects[j] = new Account(firstName, lastName, accountNumber);
+                //Account name = new Account(firstName, lastName, accountNumber);
+                break;  // // break loop so other indexes with null are not replaced
+            }
+        }
+
+        System.out.println(Arrays.toString(accountNumbers));
+        System.out.println(Arrays.toString(accountObjects));
+
+
+
+
+
+        // Menu
+        //while (!Objects.equals(name, "exit")) {
+
+
+
+
+        //}
 
 
     } // end of main method
---
+++ Account.java
@@ -0,0 +1,40 @@
+public class Account {
+
+    // Data Members
+    private String firstName;
+    private String lastName;
+    private int accountNumber;
+    private double balance;
+    private String[] transactionTypes;
+    private double[] transactionValues;
+
+
+    // General Constructor
+    public Account(String firstName, String lastName, int accountNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.accountNumber = accountNumber;
+    }   // End of General Constructor
+
+    // Default Constructor
+    public Account() {
+        // Default Values
+        this.firstName = "Unknown";
+        this.lastName = "Unknown";
+        this.accountNumber = 0;
+        this.balance = 0;
+        this.transactionTypes = new String[5];
+        this.transactionValues = new double[5];
+    }   // End of Default Constructor
+
+
+    // Methods
+    //public boolean isOwner(String name) {
+        //if
+   // }
+
+
+
+
+
+}
